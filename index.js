4b292c09bf0f72a5b439cb2298da4ab91736772b
Mfonobong John Ekott <ekottmfon@yahoo.com>
2022-07-15 23:07:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.get("/api/hello", (req, res) => {
   return res.send("success");
 });
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
   console.log("app running");
 });
